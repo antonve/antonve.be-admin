6fdc85d579985773b10a0b507e3a8414b30c1400
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-01-31 20:36:29 +0900

    add login page

--- src/app/hooks/withAuth.ts
+++ src/app/hooks/withAuth.ts
@@ -1,23 +1,26 @@
 export function withAuth(gssp) {
-  return async (context) => {
+  return async context => {
     const token = context.req.cookies['auth_token']
 
     if (!token) {
       return {
         redirect: {
-          destination: '/login'
-        }
-      };
+          destination: '/login',
+        },
+      }
     }
 
-    const gsspData = await gssp(context); // Run `getServerSideProps` to get page-specific data
+    const gsspData = await gssp(context) // Run `getServerSideProps` to get page-specific data
 
     return {
       props: {
         ...gsspData.props,
         token,
-      }
-    };
+      },
+    }
   }
 }
 
+export function storeAuthToken(token: string) {
+  document.cookie = `auth_token=${token}`
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,34 @@
+import { FormEvent, useState } from 'react'
 import type { NextPage } from 'next'
+import Router from 'next/router'
 import { Label, TextInput } from '@app/components/Form'
+import Button from '@app/components/Button'
+import { storeAuthToken } from '@app/hooks/withAuth'
 
 const Login: NextPage = () => {
+  const [password, setPassword] = useState('')
+  const saveToken = (e: FormEvent) => {
+    e.preventDefault()
+    if (password == "") {
+      return
+    }
+
+    storeAuthToken(password)
+    Router.push('/')
+  }
+
   return (
     <div className="">
-      <form>
+      <form onSubmit={saveToken}>
         <div className="mb-2">
           <Label htmlFor="password">Password</Label>
-          <TextInput id="password" value={''} onChange={() => {}} />
+          <TextInput id="password" value={password} onChange={setPassword} />
         </div>
+        <div className="flex justify-end gap-x-4">
+            <Button type="submit" primary>
+              Log in
+            </Button>
+          </div>
       </form>
     </div>
   )
