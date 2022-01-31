6bd62bcda978678b5bbd2b61b4e0d022db57a8e9
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-01-31 20:05:04 +0900

    add login hook

---
+++ src/app/hooks/withAuth.ts
@@ -0,0 +1,23 @@
+export function withAuth(gssp) {
+  return async (context) => {
+    const token = context.req.cookies['auth_token']
+
+    if (!token) {
+      return {
+        redirect: {
+          destination: '/login'
+        }
+      };
+    }
+
+    const gsspData = await gssp(context); // Run `getServerSideProps` to get page-specific data
+
+    return {
+      props: {
+        ...gsspData.props,
+        token,
+      }
+    };
+  }
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
+import { withAuth } from '@app/hooks/withAuth'
 
 const Home: NextPage = () => {
   return <div className="">Hello admin</div>
 }
+
+export const getServerSideProps = withAuth(() => {})
 
 export default Home
